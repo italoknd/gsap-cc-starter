4980f73e6d4311acd25d245a5c279e058b5a9c36
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-07-09 17:33:10 -0400

    avançando no curso

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  // é o contrario do gsap.to(), ele inicia a animação já de um ponto X para um ponto Y
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'power1.inOut'
+    });
+  }, []);
 
   return (
     <main>
--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,21 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+  //GSAP fromTo é um shorthand para os hooks from e to
+
+  //a diferença nesse caso é que ao invés de usarmos apenas 1 objeto para animar, usaremos 2 objetos
+
+  //o primeiro é o "from", ou seja, como e a partir de que ponto vai iniciar a animação
+
+  //o segundo objeto se refere ao "to", que é para onde a animação vai seguir e como ela vai terminar
+  useGSAP(() => {
+    gsap.fromTo(
+      "#red-box",
+ 
+    );
+  }, []);
 
   return (
     <main>
